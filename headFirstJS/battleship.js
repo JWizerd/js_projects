4270a5070a2fe8d82d3ec6878ef2476f58b21142
Jeremiah <jwodke@outlook.com>
2016-04-15 17:00:40 -0600

    modified battleship game

--- headFirstJS/battleship.js
+++ headFirstJS/battleship.js
@@ -1,3 +1,38 @@
-view = {
-  
-}
+
+var view = {
+  displayMessage: function(msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function(location){
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function(location){
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  }
+};
+
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+  ships: [
+{
+  locations: ["10", "20", "30"],
+  hits:["","",""]
+},
+{
+  locations: ["32", "33", "34"],
+  hits:["","",""]
+},
+{
+  locations: ["63", "64", "65"],
+  hits:["","","hit"]
+}]
+};
+
+
+
